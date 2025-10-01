921989827a5b246d486d1bd0f28ede8410835f76
Aktobias <tobias.gh.akermark@gmail.com>
2025-10-01 23:01:30 +0200

    updated gitignore

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18b6e6ce78dab21e9f4f467121f12ec71d98b71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138d75cf698d795af08ce5076699d75ff5a40af0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
